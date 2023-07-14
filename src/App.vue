593b1060817c5e286c8e287383615f49ef6a7aa9
Scissord <mrnurdik@mail.ru>
2023-07-15 00:58:35 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <navigation-bar></navigation-bar>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavigationBar from "@/components/NavigationBar.vue";
+
+export default {
+  components: {NavigationBar}
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,36 @@
+import MainPage from "@/pages/MainPage.vue";
+import {createRouter, createWebHistory} from "vue-router";
+import PostPage from "@/pages/PostPage.vue";
+import PostIdPage from "@/pages/PostIdPage.vue";
+import PostPageWithStore from "@/pages/PostPageWithStore.vue";
+import PostPageCompositionApi from "@/pages/PostPageCompositionApi.vue";
+
+const routes = [
+    {
+        path: '/',
+        component: MainPage
+    },
+    {
+        path: '/posts',
+        component: PostPage
+    },
+    {
+        path: '/posts/:id',
+        component: PostIdPage
+    },
+    {
+        path: '/store',
+        component: PostPageWithStore
+    },
+    {
+        path: '/composition',
+        component: PostPageCompositionApi
+    },
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(process.env.BASE_URL)
+});
+
+export default router
